5010f059f25309696bb228864f945d16a9c1cefe
Andrii <avdovyniuk@gmail.com>
2026-07-27 21:01:10 -0500

    Continuing work and research on SQL tables and policies

--- supabase/migrations/20260726224359_initial-schema.sql
+++ supabase/migrations/20260726224359_initial-schema.sql
@@ -1,23 +1,29 @@
 -- TODO No data sources are configured to run this SQL and provide advanced code assistance
 
 --Notebook table
-
+create table if not exists Notebook(
+   --all the auth related stuff would probably go there
+);
 
 --Deck table
+create table if not exists Deck(
+    id serial not null unique,
+    name varchar(255) not null unique,  --I dont think the actual code enforces that decks have unique names yet
 
+);
 
 --Flashcard table
-
-
 --TODO make sure to implement rules, ex: Row level security to the program
 
 create table if not exists FlashCard(
     id serial not null unique,
     title varchar(255) not null unique,
     info varchar(255) not null unique,
-    -- TODO add a tie back to the owning class (EX: deckName -> deck that owns the card
+    owner varchar(255) not null,            --references the deck that owns the card
 
-    --TODO add a foreign key constraint on the owning class - When deck is deleted the card is also deleted
-    foreign key(owner) references Dog(userName)
+
+    -- foreign key constraint on the owning class - When deck is deleted the card is also deleted
+    --TODO maybe have it as id?
+    foreign key(owner) references Deck(name)
     on delete cascade
-    );
+ );
